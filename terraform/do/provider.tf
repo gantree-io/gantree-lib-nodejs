22885490b555aaeccbd7bfd6e635e148194e7560
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-02-11 11:34:52 +1100

    use envvar for DO token

--- terraform/do/provider.tf
+++ terraform/do/provider.tf
@@ -1,9 +1,10 @@
-# variable "digitalocean_token" {}
+variable "digitalocean_token" {}
 # variable "digitalocean_ssh_fingerprint" {}
 # variable "digitalocean_pub_key" {}
 # variable "digitalocean_private_key" {}
 
 provider "digitalocean" {
-  token   = "44166eb03ee15b7826ecdae4c2a729f905ea5e623cb78bf80a10e0d21187d757"
+  # token   = "44166eb03ee15b7826ecdae4c2a729f905ea5e623cb78bf80a10e0d21187d757"
+  token   = var.digitalocean_token
   version = "~> 1.13"
 }
