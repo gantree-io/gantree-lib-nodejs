40867582818a06cf65198b696a0c1f818c75e94f
Denver <drtexxofficial@gmail.com>
2020-02-17 19:05:46 +1100

    tpl.js - trim code

--- src/lib/tpl.js
+++ src/lib/tpl.js
@@ -1,26 +1,15 @@
-const fs = require('fs-extra');
-const Handlebars = require('handlebars');
-// const path = require('path');
+const fs = require('fs-extra')
+const Handlebars = require('handlebars')
 
-
-Handlebars.registerHelper('raw', function (options) {
-  return options.fn();
-});
+Handlebars.registerHelper('raw', function(options) {
+  return options.fn()
+})
 
 module.exports = {
   create: (source, target, data) => {
-    const sourceTpl = fs.readFileSync(source).toString();
-    const template = Handlebars.compile(sourceTpl);
-    const contents = template(data);
-
-    // console.log({data})
-
-    // const targetDir = path.dirname(target);
-    // console.log({source, target});
-    // fs.rmdirSync(targetDir, {recursive: true});
-    // fs.mkdirSync(targetDir, {recursive: true});
-
-    // console.log(targetDir)
-    fs.writeFileSync(target, contents);
+    const sourceTpl = fs.readFileSync(source).toString()
+    const template = Handlebars.compile(sourceTpl)
+    const contents = template(data)
+    fs.writeFileSync(target, contents)
   }
 }
