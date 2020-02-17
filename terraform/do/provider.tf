bb10860ca8d13180e0965ab48df921a210ae364c
Denver <drtexxofficial@gmail.com>
2020-02-18 10:24:11 +1100

    [do] clean up unused code

--- terraform/do/provider.tf
+++ terraform/do/provider.tf
@@ -1,9 +1,3 @@
-# variable "digitalocean_ssh_fingerprint" {}
-# variable "digitalocean_pub_key" {}
-# variable "digitalocean_private_key" {}
-
 provider "digitalocean" {
-  # token   = "44166eb03ee15b7826ecdae4c2a729f905ea5e623cb78bf80a10e0d21187d757"
-  # token   = "${digitalocean_token}"
   version = "~> 1.13"
 }
