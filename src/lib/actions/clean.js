865bbf74fd5202243789c5e27ea733cbeeec8ef3
Denver <drtexxofficial@gmail.com>
2020-02-19 11:38:11 +1100

    clean - validate config prior to running

--- src/lib/actions/clean.js
+++ src/lib/actions/clean.js
@@ -1,27 +1,28 @@
-const chalk = require('chalk');
-const process = require('process');
+const chalk = require('chalk')
+const process = require('process')
 
-const config = require('../config.js');
-const { Platform } = require('../platform.js');
-
+const config = require('../config.js')
+const { Platform } = require('../platform.js')
 
 module.exports = {
-  do: async (cmd) => {
+  do: async cmd => {
     if (!cmd.config) {
-      console.info('--config required.');
-      process.exit(1);
+      console.info('--config required.')
+      process.exit(1)
     }
 
-    const cfg = config.read(cmd.config);
+    const cfg = config.read(cmd.config)
 
-    console.log(chalk.yellow('[Gantree] Cleaning platform...'));
-    const platform = new Platform(cfg);
+    config.validate(cfg)
+
+    console.log(chalk.yellow('[Gantree] Cleaning platform...'))
+    const platform = new Platform(cfg)
     try {
-      await platform.clean();
+      await platform.clean()
     } catch (e) {
-      console.log(chalk.red(`[Gantree] Could not clean platform: ${e.message}`));
-      process.exit(-1);
+      console.log(chalk.red(`[Gantree] Could not clean platform: ${e.message}`))
+      process.exit(-1)
     }
-    console.log(chalk.green('[Gantree] Done'));
+    console.log(chalk.green('[Gantree] Done'))
   }
 }
