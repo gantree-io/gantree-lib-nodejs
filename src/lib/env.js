3b33d7f65e77179b381e1c382e2ad558b6a78ba1
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-02-28 15:20:40 +1100

    semicolon purge

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,6 +1,6 @@
 module.exports = {
   validatorSshPrivateKeyPath: process.env.SSH_ID_RSA_VALIDATOR,
-  nodeExporterUsername: process.env.NODE_EXPORTER_USERNAME || "prometheus",
-  nodeExporterPassword: process.env.NODE_EXPORTER_PASSWORD || "node_exporter",
+  nodeExporterUsername: process.env.NODE_EXPORTER_USERNAME || 'prometheus',
+  nodeExporterPassword: process.env.NODE_EXPORTER_PASSWORD || 'node_exporter',
   terraformStatefilePath: process.env.TERRAFORM_STATEFILE_PATH
-};
+}
