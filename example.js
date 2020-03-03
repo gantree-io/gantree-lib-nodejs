a873013a4c07fa07a3fe2e53ef18f9a076457786
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-03 11:49:11 +1100

    [example.js] remove most logs, add comments

--- example.js
+++ example.js
@@ -4,26 +4,20 @@ const PATH_TO_CONFIG =
   '/home/denver/github/gantree-cli/samples/config/cheap_aws.sample.json'
 
 async function main() {
-  console.log('[+0] main started.')
-
+  // instansiate a new Gantree object
   const gantree = new Gantree()
 
-  console.log('[+1] get config started.')
-
+  // load config into function and validate
   let my_config = await gantree.returnConfig(PATH_TO_CONFIG)
 
-  console.log('[-1] get config finished.')
-  console.log('[+1] sync platform started.')
-
+  // sync infrastructure with config
   let infraObj = await gantree.syncPlatform(my_config)
 
-  console.log('[-1] sync platform finished.')
-  console.log('[+1] sync application started.')
-
+  // convert instances into respective nodes/validators
   await gantree.syncApplication(my_config, infraObj)
 
-  console.log('[-1] sync application finished.')
-  console.log('[-0] main finished.')
+  // finished
+  console.log('DONE.')
 }
 
 main()
