de1490788175d3403c02d12ad635f119e1be9aca
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-04 12:35:42 +1100

    [platform.js] require config

--- src/lib/platform.js
+++ src/lib/platform.js
@@ -1,8 +1,15 @@
 const asyncUtils = require('./async.js')
 const { Terraform } = require('./clients/terraform')
+const { throwGantreeError } = require('./error')
 
 class Platform {
   constructor(cfg) {
+    if (cfg === undefined) {
+      throwGantreeError(
+        'MISSING_ARGUMENTS',
+        Error('Platform requires a config')
+      )
+    }
     this.config = JSON.parse(JSON.stringify(cfg))
 
     this.tf = new Terraform(this.config)
