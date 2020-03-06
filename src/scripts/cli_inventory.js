130ac6756a3aa1b3c67dc9cf66134b4d975a9e79
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-06 17:50:43 +1100

    [cli_inventory] add extra checks

--- src/scripts/cli_inventory.js
+++ src/scripts/cli_inventory.js
@@ -1,5 +1,25 @@
 #!/usr/bin/env node
-
+const chalk = require('chalk')
+const config = require('../lib/config')
+const check = require('../lib/check')
 const { inventory } = require('../lib/dataManip/inventory')
 
-inventory()
+async function main() {
+  const configPath = process.env.GANTREE_INVENTORY_CONFIG_PATH
+
+  if (!configPath) {
+    console.error(
+      chalk.red('[Gantree] Error: env|GANTREE_INVENTORY_CONFIG_PATH required.')
+    )
+    process.exit(-1)
+  }
+
+  const gantreeConfigObj = config.read(configPath)
+
+  await config.validate(gantreeConfigObj)
+  await check.envVars(gantreeConfigObj)
+
+  inventory(gantreeConfigObj)
+}
+
+main()
