c15be07a6d16de5ed045f3a2771427f457bb4b9b
Ryan Miller <ryan.miller@flexdapps.com>
2020-03-11 17:28:22 +1100

    add default true deletion_protection for gcp

--- src/lib/dataManip/inventory.js
+++ src/lib/dataManip/inventory.js
@@ -150,7 +150,7 @@ const parseNode = (name, item, config) => {
         item.instance.sourceImage ||
         'projects/ubuntu-os-cloud/global/images/family/ubuntu-1804-lts',
       size_gb: item.instance.sizeGb || 50,
-      deletion_protection: item.instance.deletionProtection,
+      deletion_protection: item.instance.deletionProtection || true,
       zone: item.instance.zone,
       region: item.instance.region,
       ssh_user: item.instance.sshUser,
