95b4c98e75d5c8f185938f62e369ce22f25ad79d
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-21 18:05:12 +1100

    [lib/ansible/inventory] add createGantreeInventory method

--- src/lib/ansible/inventory.js
+++ src/lib/ansible/inventory.js
@@ -1,5 +1,9 @@
 const path = require('path')
 const fs = require('fs')
+const { inventory } = require('../dataManip/inventory')
+const { Paths } = require('../utils/paths')
+
+const paths = new Paths()
 
 async function createNamespace(projectPath) {
   console.log('...creating namespace')
@@ -17,8 +21,48 @@ async function createNamespace(projectPath) {
   console.log('...created namespace!')
 }
 
+async function createGantreeInventory(gantreeConfigObj, projectPath) {
+  console.log('...creating gantree inventory')
+
+  const gantreeInventoryPath = await path.join(projectPath, 'gantree')
+  const gantreeInventoryFilePath = await path.join(
+    projectPath,
+    'gantreeInventory.json'
+  )
+  const gantreeShFilePathSrc = await paths.getInventoryPath('gantree.sh')
+  const gantreeShFilePathTarget = await path.join(
+    gantreeInventoryPath,
+    'gantree.sh'
+  )
+  const projectPathTxtFilePath = await path.join(
+    gantreeInventoryPath,
+    'project_path.txt'
+  )
+
+  // turn config object into a gantree inventory
+  const gantreeInventoryObj = await inventory(gantreeConfigObj, projectPath)
+
+  // write the gantree inventory to inventory/{NAMESPACE}/gantreeInventory.json
+  await fs.writeFileSync(
+    gantreeInventoryFilePath,
+    JSON.stringify(gantreeInventoryObj),
+    'utf8'
+  )
+
+  // copy gantree.sh into inventory/{NAMESPACE}/gantree/
+  fs.copyFileSync(gantreeShFilePathSrc, gantreeShFilePathTarget)
+
+  // write project's path to project_path.txt (used as CLI argument)
+  await fs.writeFileSync(projectPathTxtFilePath, `${projectPath}`, 'utf8')
+
+  console.log('...created gantree inventory!')
+
+  return gantreeInventoryPath
+}
+
 module.exports = {
-  createNamespace
+  createNamespace,
+  createGantreeInventory
 }
 
 // const projectName = gantreeConfigObj.metadata.project
