383bd1bce15a1c3ac08c15beebc68a21ac98370d
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-23 12:13:25 +1100

    add ansible/extract

---
+++ src/lib/ansible/extract.js
@@ -0,0 +1,47 @@
+async function IPs(combinedInventoryObj, _options = {}) {
+  const verbose = _options.verbose || false
+
+  if (verbose === true) {
+    console.log('...extracting IPs')
+  }
+
+  // TODO: ansible code must be changed from "validator" to "node" group
+  const nodeHostNames = combinedInventoryObj.validator.children
+
+  if (verbose === true) {
+    console.log(nodeHostNames)
+  }
+
+  let allNodeIps = []
+
+  for (const hostName of nodeHostNames) {
+    // use names to pull out hostname objects
+    const hostNamesProperties = combinedInventoryObj[hostName]
+    const hostNameIps = hostNamesProperties.hosts
+    if (verbose === true) {
+      console.log(`----properties of hostname '${hostName}':----`)
+      console.log(hostNamesProperties)
+      console.log('--IPs--')
+      console.log(hostNameIps)
+    }
+    hostNameIps.forEach(ip_n => {
+      allNodeIps.push(ip_n)
+    })
+  }
+  if (verbose === true) {
+    console.log('ALL IPs:')
+    console.log(allNodeIps)
+  }
+
+  //convert inventoryOutput into NodeIpAddresses
+
+  if (verbose === true) {
+    console.log('...extracted IPs!')
+  }
+
+  return allNodeIps
+}
+
+module.exports = {
+  IPs
+}
--- src/lib/ansible/index.js
+++ src/lib/ansible/index.js
@@ -1,10 +1,12 @@
 const commands = require('./commands')
 const inventory = require('./inventory')
+const extract = require('./extract')
 
 class Ansible {
   constructor() {
     this.commands = commands
     this.inventory = inventory
+    this.extract = extract
     // stuff
   }
 }
