36fc8391eb30bc0c535c9d4ed307c4909346682f
Ryan Miller <ryan.miller@flexdapps.com>
2020-03-24 16:40:02 +1100

    update lib test_ files

--- src/test_sync.js
+++ src/test_sync.js
@@ -1,4 +1,8 @@
+const { Config } = require('./lib/config')
 const { Gantree } = require('./lib/gantree')
 
+const config = new Config()
+const cfg = config.read(process.env.GANTREE_CONFIG_PATH)
+
 const gantree = new Gantree()
-gantree.ansibleSyncAll()
+gantree.syncAll(cfg)
