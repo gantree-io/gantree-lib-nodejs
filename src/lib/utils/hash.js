da3f238646553a568f1a608c5915ba8052fc0cea
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-25 16:18:59 +1100

    add utils/hash.js

---
+++ src/lib/utils/hash.js
@@ -0,0 +1,52 @@
+const crypto = require('crypto')
+
+function getChecksum(dataStr, algorithm, encoding) {
+  return crypto
+    .createHash(algorithm || 'sha256')
+    .update(dataStr, 'utf8')
+    .digest(encoding || 'hex')
+}
+
+function validateChecksum(
+  dataStr,
+  expectedChecksum,
+  algorithm,
+  encoding,
+  _options = {}
+) {
+  // note: newlines and whitespace considered in checksum
+  const verbose = _options.verbose || false
+
+  const hashA = getChecksum(dataStr, algorithm, encoding)
+  const hashB = expectedChecksum
+  if (verbose === true) {
+    console.log(`A: ${hashA}`)
+    console.log(`B: ${hashB}`)
+  }
+
+  if (hashA === hashB) {
+    if (verbose === true) {
+      console.log('checksum match.')
+    }
+    return true
+  } else {
+    if (verbose === true) {
+      console.log('checksum mismatch!')
+    }
+    return false
+  }
+}
+
+// function getFileChecksum(filePath, algorithm, encoding) {
+//     const dataStr = fs.readFileSync(filePath, 'utf-8')
+//     return getChecksum(dataStr, algorithm, encoding)
+// }
+
+const hash = {
+  getChecksum,
+  validateChecksum
+}
+
+module.exports = {
+  hash
+}
