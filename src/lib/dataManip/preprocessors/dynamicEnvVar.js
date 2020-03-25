a5320538ac22e1895e110c981bb286fa36ec2347
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-25 17:52:21 +1100

    [dynamicEnvVar] error handle missing env vars

--- src/lib/dataManip/preprocessors/dynamicEnvVar.js
+++ src/lib/dataManip/preprocessors/dynamicEnvVar.js
@@ -1,4 +1,5 @@
 const cloneDeepWith = require('lodash.clonedeepwith')
+const { throwGantreeError } = require('../../error')
 
 const envRegex = /(?<=^\$(env|ENV):)[A-Za-z_]+(?=$)/
 
@@ -6,7 +7,15 @@ function dynamicEnvVarCustomizer(value) {
   if (typeof value === 'string') {
     const match = value.match(envRegex)
     if (match) {
-      return process.env[match[0]] || ''
+      const exists = process.env.hasOwnProperty(match[0])
+      if (exists) {
+        return process.env[match[0]]
+      } else {
+        throwGantreeError(
+          'ENVIRONMENT_VARIABLE_MISSING',
+          Error(`Failed to resolve reference in Gantree config: '${value}'`)
+        )
+      }
     }
   }
 }
