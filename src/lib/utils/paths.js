919641802acb3a033217fd5fe365380c37266080
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-31 15:31:28 +1100

    [paths] add override option for inventoryPath

--- src/lib/utils/paths.js
+++ src/lib/utils/paths.js
@@ -1,6 +1,7 @@
 // NOTE: Be very, very careful moving this file!
 
 const path = require('path')
+const opt = require('./options')
 
 class Paths {
   constructor() {
@@ -11,6 +12,7 @@ class Paths {
     return path.join(__dirname, '../', '../', '../', ...extra)
   }
 
+  // note: does not consider overrides, handled in getProjectPath
   getInventoryPath(...extra) {
     return this.getGantreePath('inventory', ...extra)
   }
@@ -19,8 +21,19 @@ class Paths {
     return this.getGantreePath('inventorySegments', ...extra)
   }
 
-  getProjectPath(projectName) {
-    return this.getGantreePath('inventory', projectName)
+  getProjectPath(projectName, _options = {}) {
+    const inventoryPathOverride = opt.default(
+      _options.inventoryPathOverride,
+      undefined
+    )
+
+    if (inventoryPathOverride === undefined) {
+      // use gantree inventory path as base
+      return this.getInventoryPath(projectName)
+    } else {
+      // use override inventory path as base
+      return path.join(inventoryPathOverride, projectName)
+    }
   }
 
   getPlaybookFilePath(playbookFilename) {
