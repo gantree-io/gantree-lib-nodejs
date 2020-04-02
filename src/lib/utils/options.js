59ba17045efff5eadf465a6026c97a872f5eecdf
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-04-02 13:18:00 +1100

    [utils/options] add optional callback if val defaulted

--- src/lib/utils/options.js
+++ src/lib/utils/options.js
@@ -1,5 +1,14 @@
-function getDefault(real_value, default_value) {
+/**
+ * Return a default value if the real one is undefined
+ * @param {*} real_value - actual value of option (could be undefined)
+ * @param {*} default_value - default value if undefined
+ * @param {function} [execIfDefaulted] - optional function to run if default was used
+ */
+function getDefault(real_value, default_value, execIfDefaulted) {
   if (real_value === undefined) {
+    if (execIfDefaulted !== undefined) {
+      execIfDefaulted()
+    }
     return default_value
   } else {
     return real_value
