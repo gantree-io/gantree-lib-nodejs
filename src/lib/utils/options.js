1b8c66129e87a5bf422b125fd9e1c10aed653e3f
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-04-03 13:47:13 +1100

    [utils/options] add required method

--- src/lib/utils/options.js
+++ src/lib/utils/options.js
@@ -1,3 +1,5 @@
+const { throwGantreeError } = require('../error')
+
 /**
  * Return a default value if the real one is undefined
  * @param {*} real_value - actual value of option (could be undefined)
@@ -15,6 +17,29 @@ function getDefault(real_value, default_value, execIfDefaulted) {
   }
 }
 
+/**
+ * Return value if defined, otherwise throw error
+ * @param {*} real_value - actual value of option (could be undefined)
+ * @param {function} [execIfMissing] - optional function to run if value is undefined
+ */
+function getRequired(real_value, execIfMissing) {
+  if (real_value === undefined) {
+    // exec callback
+    execIfMissing()
+    // if callback didn't return non-zero exit-code
+    // print trace
+    console.trace()
+    // throw backup error
+    throwGantreeError(
+      "INTERNAL_ERROR",
+      Error("unhandled required fail, see trace above (callback function used should return non-zero exit-code)")
+    )
+  } else {
+    return real_value
+  }
+}
+
 module.exports = {
-  default: getDefault
+  default: getDefault,
+  required: getRequired
 }
