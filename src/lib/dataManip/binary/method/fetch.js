9edb351962845a1fa3a8f9179c29dde26cb9f85d
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-04-06 10:31:18 +1000

    [dataManip] finish fetch method

--- src/lib/dataManip/binary/method/fetch.js
+++ src/lib/dataManip/binary/method/fetch.js
@@ -1,6 +1,22 @@
-function resolveFetch() {
+const opt = require('../../../utils/options')
+const { throwGantreeError } = require('../../../error')
+
+function missingUrl() {
+  throwGantreeError('BAD_CONFIG', Error('fetch method requires a url'))
+}
+
+/**
+ * resolve defaults for fetch key in Gantree config
+ * @param {object} fetchObj - value of fetch key in Gantree config
+ * @returns {object} return inventory keys for fetch method
+ */
+function resolveFetch(fetchObj) {
+  fetchObj.url = opt.required(fetchObj.url, missingUrl)
+  fetchObj.sha256 = opt.default(fetchObj.sha256, 'false')
+
   const invKeys = {
-    substrate_binary_url: 'PLACEHOLDER'
+    substrate_binary_url: fetchObj.url,
+    substrate_binary_sha256: fetchObj.sha256
   }
   return invKeys
 }
