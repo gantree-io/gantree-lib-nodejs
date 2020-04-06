f05ec4aed6df73c13c33ee4d040509089244395b
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-04-06 10:37:33 +1000

    [dataManip] local placeholder

--- src/lib/dataManip/binary/method/local.js
+++ src/lib/dataManip/binary/method/local.js
@@ -1,8 +1,13 @@
 function resolveLocal() {
-    console.log("LOCAL IS WIP")
-    process.exit(1)
+  // TODO: inventory keys for local, info for once implemented in ansible
+  // const invKeys = {
+  //     substrate_binary_path: binaryObj.local.path,
+  //     substrate_binary_sha256: fetchObj.sha256
+  // }
+  console.log('LOCAL BINARY METHOD IS WIP')
+  process.exit(1)
 }
 
 module.exports = {
-    resolveLocal
+  resolveLocal
 }
