153ecc03749f9874148f3f9415377452f66d394c
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-04-16 17:46:06 +1000

    [dataManip/binary] improve binary workaround code

--- src/lib/dataManip/binary/index.js
+++ src/lib/dataManip/binary/index.js
@@ -6,11 +6,31 @@ function missingFilename() {
   throwGantreeError('BAD_CONFIG', Error('you must specify a binary filename'))
 }
 
+// ideally this function shouldn't need to return anything
+// however, if no workarounds needed, do not delete!
+/**
+ * Returns inventory keys needed for workarounds. Function may return a dict or nothing depending on workarounds required.
+ * @param {Object} binaryObj - binary object from Gantree config
+ */
 function resolveWorkaroundInvKeys(binaryObj) {
-  const workaroundInvKeys = {
-    edgeware: binaryObj.edgeware, // TODO(ryan): remove this special case once edgeware spec is fixed
+  const staticKeys = {
     substrate_purge_chain: 'true' // TODO(ryan): add more complex purge mechanics
   }
+  let derivedKeys = {
+    edgeware: binaryObj.edgeware // TODO(ryan): remove this special case once edgeware spec is fixed
+  }
+
+  // if binaryObj has a repository key
+  if (Object.prototype.hasOwnProperty.call(binaryObj, 'repository')) {
+    derivedKeys['substrate_binary_url'] = 'false' // TODO(Denver): ansible needs to handle if this variable is undefined
+  }
+
+  const workaroundInvKeys = {
+    ...staticKeys,
+    ...derivedKeys
+  }
+  // TODO(Denver): add flag for printing workaround keys, will be easier to debug any unexpected behaviour in future
+  // console.log(`Current workaround inventory keys: ${JSON.stringify(workaroundInvKeys)}`)
   return workaroundInvKeys
 }
 
