8a45d42facde14f03a31239c6e097ad5e5fb2cf4
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-04-16 17:47:30 +1000

    Simplify and fix binary method resolution

--- src/lib/dataManip/binary/method/index.js
+++ src/lib/dataManip/binary/method/index.js
@@ -2,31 +2,32 @@ const { resolveRepository } = require('./repository')
 const { resolveFetch } = require('./fetch')
 const { resolveLocal } = require('./local')
 const { resolvePreset } = require('./preset')
-
-// define resolve functions for each binary method
-const resolverLookup = {
-  repository: resolveRepository,
-  fetch: resolveFetch,
-  local: resolveLocal,
-  preset: resolvePreset
-}
+const { throwGantreeError } = require('../../../error')
 
 /**
+ * Returns the inventory keys for the given binary method
  * @param {object} binaryObject - value of the binary key in Gantree configuration
- * @returns {object} ansible inventory binary keys
+ * @returns {object} ansible inventory binary method keys
  */
 function resolveInvKeys(binaryObj) {
   let invKeys = {}
-  // for each resolver defined
-  for (const [k, v] of Object.entries(resolverLookup)) {
-    // if the binary object contains a key also in the resolvers
-    if (Object.prototype.hasOwnProperty.call(binaryObj, k)) {
-      // console.log(`${k} in ${JSON.stringify(binaryObj)}`)
-      // use the resolver matching this key
-      const resolvedInvKeys = v(binaryObj[k])
-      Object.assign(invKeys, resolvedInvKeys)
-    }
+  if (Object.prototype.hasOwnProperty.call(binaryObj, 'repository')) {
+    invKeys = resolveRepository(binaryObj)
+  } else if (Object.prototype.hasOwnProperty.call(binaryObj, 'fetch')) {
+    invKeys = resolveFetch(binaryObj)
+  } else if (Object.prototype.hasOwnProperty.call(binaryObj, 'local')) {
+    invKeys = resolveLocal(binaryObj) // throw intentional error
+  } else if (Object.prototype.hasOwnProperty.call(binaryObj, 'preset')) {
+    invKeys = resolvePreset(binaryObj) // throw intentional error
+  } else {
+    throwGantreeError(
+      'INTERNAL_ERROR',
+      Error(
+        'no matching binary methods found in config, this should have been handled during schema validation. Please open a Github issue if you see this.'
+      )
+    )
   }
+
   return invKeys
 }
 
