570f0022926cdf1b0b4bc8991662d02ff9e12d6d
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-04-16 17:36:01 +1000

    throw error when trying to use local binary

--- src/lib/dataManip/binary/method/local.js
+++ src/lib/dataManip/binary/method/local.js
@@ -1,11 +1,15 @@
+const { throwGantreeError } = require('../../../error')
+
 function resolveLocal() {
   // TODO: inventory keys for local, info for once implemented in ansible
   // const invKeys = {
   //     substrate_binary_path: binaryObj.local.path,
   //     substrate_binary_sha256: fetchObj.sha256
   // }
-  console.log('LOCAL BINARY METHOD IS WIP')
-  process.exit(1)
+  throwGantreeError(
+    'UNSUPPORTED_OPERATION',
+    Error('Local binary method is not yet supported')
+  )
 }
 
 module.exports = {
